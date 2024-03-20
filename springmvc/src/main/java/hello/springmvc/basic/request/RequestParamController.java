b906229006e2ae1e8b9fcb8712b60a9912fcc451
hyrki09 <hyrki0901@naver.com>
2024-03-20 23:05:37 +0900

    HTTP요청 - @ModelAttribute

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,9 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -78,6 +80,40 @@ public class RequestParamController {
 
         log.info("username={}, age={}",paramMap.get("username"),paramMap.get("age"));
         return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData){
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+//        스프링MVC는 ModelAttribute가 있으면
+        // 1. HelloData 객체를 생성 2. 요청파라미터 이름으로 HelloData 객체의 프로퍼티를 찾는다. setter를 호출 후 파라미터의 값을 바인딩
+        // 3. 파라미터 이름 : username  setUsername()호출
+
+        //**프로퍼티**
+        //객체에 `getUsername()` , `setUsername()` 메서드가 있으면, 이 객체는 `username` 이라는 프로퍼티를 가지고 있 다.
+
+        log.info("username={}, age={}",helloData.getUsername(),helloData.getAge());
+        log.info("helloData=",helloData);
+
+        return  "ok";
+    }
+
+
+    /**
+     * @ModelAttribute 생략 가능
+     * String, int 같은 단순 타입 = @RequestParam
+     * argument resolver 로 지정해둔 타입 외 = @ModelAttribute */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData){
+
+        log.info("username={}, age={}",helloData.getUsername(),helloData.getAge());
+        log.info("helloData=",helloData);
+
+        return  "ok";
     }
 
 
