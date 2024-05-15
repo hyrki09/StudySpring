aa1c73d7457f675a240401e8a3b304e048c5037c
hyrki09 <hyrki0901@naver.com>
2024-05-15 12:54:00 +0900

    세션 - 서블릿 HTTP세션

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.websocket.Session;
 
 @Slf4j
@@ -45,7 +46,7 @@ public class HomeController {
 
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model){
 
         // 세션 관리자에 저장된 회원 정보 조회
@@ -57,6 +58,27 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3(HttpServletRequest request, Model model){
+
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member)session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        //
+        model.addAttribute("member", loginMember);
         return "loginHome";
 
     }
