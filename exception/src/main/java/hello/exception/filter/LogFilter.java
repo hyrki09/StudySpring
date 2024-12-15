117c2912add2c0b146f7af04ba472df490cf233e
hyrki09 <hyrki0901@naver.com>
2024-12-15 16:59:01 +0900

    exception - 서블릿 예외처리 (interceptor)

--- exception/src/main/java/hello/exception/filter/LogFilter.java
+++ exception/src/main/java/hello/exception/filter/LogFilter.java
@@ -1,47 +1,43 @@
 package hello.exception.filter;
 
 
-import jakarta.servlet.Filter;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.UUID;
 
 
 @Slf4j
-public class LogFilter implements HandlerInterceptor {
-
-    public static final String LOG_ID = "logId";
-
+public class LogFilter implements Filter {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse
-            response, Object handler) throws Exception {
-        String requestURI = request.getRequestURI();
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
         String uuid = UUID.randomUUID().toString();
-        request.setAttribute(LOG_ID, uuid);
-        log.info("REQUEST  [{}][{}][{}][{}]", uuid, request.getDispatcherType(),
-                requestURI, handler);
-        return true;
-    }
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse
-            response, Object handler, ModelAndView modelAndView) throws Exception {
-        log.info("postHandle [{}]", modelAndView);
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse
-            response, Object handler, Exception ex) throws Exception {
-        String requestURI = request.getRequestURI();
-        String logId = (String)request.getAttribute(LOG_ID);
-        log.info("RESPONSE [{}][{}][{}]", logId, request.getDispatcherType(),
-                requestURI);
-        if (ex != null) {
-            log.error("afterCompletion error!!", ex);
+        try {
+            log.info("REQUEST  [{}][{}][{}]", uuid, request.getDispatcherType(),
+                    requestURI);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("RESPONSE [{}][{}][{}]", uuid, request.getDispatcherType(),
+                    requestURI);
         }
+    }
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
     }
 
 }
