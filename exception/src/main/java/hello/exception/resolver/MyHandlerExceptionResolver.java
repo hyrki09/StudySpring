e754c581007a95d6864852f4f61d712ea250cbb0
hyrki09 <hyrki0901@naver.com>
2025-02-08 12:37:51 +0900

    HandlerExceptionResolver시작

---
+++ exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -0,0 +1,33 @@
+package hello.exception.resolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+@Slf4j
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+
+        try {
+            // 500 -> 400으로 예외처리 변경
+            if (ex instanceof IllegalArgumentException) {
+                log.info("IllegalArgumentException resolver to 400");
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+
+                return new ModelAndView(); // 빈값으로 넘기면 예외를 먹어버릴 수 있음
+            }
+        } catch (IOException e) {
+            log.error("resolver exception", e);
+//            throw new RuntimeException(e);
+        }
+
+
+        return null;
+    }
+}
